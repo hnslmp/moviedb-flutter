bc4b082fb3c824bb8620c60223450f07f09edb67
hnslmp <ancemo14@gmail.com>
2023-02-02 11:30:50 +0700

    Extract genre page and create new movie list class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:moviedb_flutter/services.dart';
-import 'package:moviedb_flutter/model/GenresResponse.dart';
+import 'genre_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,55 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  GenresResponse? genresResponse;
-  var isLoaded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchGenres();
-  }
-
-  fetchGenres() async {
-    genresResponse = await Services().fetchGenres();
-    if (genresResponse != null) {
-      setState(() {
-        isLoaded = true;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Choose your movie genre"),
-      ),
-      body: Visibility(
-        child: ListView.builder(
-            itemCount: genresResponse?.genres.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text(genresResponse!.genres[index].name),
-                trailing: const Icon(Icons.arrow_forward),
-              );
-            }),
-        replacement: const Center(
-          child: CircularProgressIndicator(),
-        ),
-      ),
-    );
+    return MaterialApp(home: GenrePage());
   }
 }
